9a80ce1955c406b361da42723869b35a2b050dfe
Darren Ha <nberserk@gmail.com>
2016-12-28 13:49:39 +0900

    services done

--- angular2-heroeditor/app/app.component.ts
+++ angular2-heroeditor/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 
 
@@ -68,35 +70,36 @@ import { Hero } from './hero';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+providers: [HeroService],
 
 
 })
 
 
-export class AppComponent  { 
+
+
+export class AppComponent implements OnInit{ 
   name = 'Angular'; 
   title = 'Tour of Heroes';
-  heroes = HEROES;
+  heroes : Hero[];
   selectedHero: Hero;
+  constructor(private heroService: HeroService){}
+
+  gerHeroes(): void{
+    this.heroService.getHeroesSlowly().then(h=> this.heroes=h);
+  }  
 
   onSelect(hero: Hero): void {
     this.selectedHero=hero;
   }
+
+  ngOnInit(): void {
+    this.gerHeroes();
+  }
 }
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+
 
 
 
---
+++ angular2-heroeditor/app/hero.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+
+@Injectable()
+export class HeroService{
+    getHeroes(): Promise<Hero[]> {
+        return Promise.resolve(HEROES);
+    }
+
+    getHeroesSlowly(): Promise<Hero[]> {       
+        return new Promise(resolve => {
+            setTimeout( ()=> resolve(this.getHeroes()), 2000 );
+        })
+    }
+}
