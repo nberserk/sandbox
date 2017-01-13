243ec91f4591edaeaa545acc18bd16b742618e10
Darren Ha <nberserk@gmail.com>
2017-01-13 14:24:20 +0900

    temp

--- angular2-heroeditor/app/app.module.ts
+++ angular2-heroeditor/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule }      from '@angular/core';
 import {RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import {HttpModule}      from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
@@ -15,6 +20,9 @@ import {AppRoutingModule} from './app-routing.module';
   imports:      [ 
     BrowserModule, 
     FormsModule,
+    AppRoutingModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ 
