779705410b6a97e207da0c4d48de493a0f1fdb78
Darren ha <nberserk@gmail.com>
2018-06-11 22:53:32 +0900

    minor

--- scrapy/kim/spiders/main_spider.py
+++ scrapy/kim/spiders/main_spider.py
@@ -11,9 +11,11 @@ class KimSpider(scrapy.Spider):
     password_key = 'PASSWORD'
     
     def after_login(self, response):
+        print response.body
         if 'location.replace' in response.body:
             #print 'raw: %s' % res
             yield scrapy.Request(url='http://kimdongjo.com/bbs/board.php?bo_table=conditions', callback=self.parse_conditions)
+
             
     def parse_conditions(self, response):
         print 'conditions: %s' % response.body
@@ -27,10 +29,8 @@ class KimSpider(scrapy.Spider):
         #     yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        #print self.settings.attributes['PASSWORD']
         print self.settings.get(self.password_key)
         return FormRequest(self.prefix+'bbs/login_check.php',
                            formdata={'mb_id': 'nberserk', 'mb_password': self.settings.get(self.password_key)}, method='POST',
-            callback=self.after_login
-        )
+            callback=self.after_login)
         
