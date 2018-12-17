54635f2514b11360bfbb1240ce86288414946c61
darren <darren.ha@samsung.com>
2018-12-17 09:00:32 +0900

    update spark sample

--- spark/README.md
+++ spark/README.md
@@ -5,6 +5,8 @@
 - spark version : [spark v2.3.2 Apache Haddop 2.7 and later](http://spark.apache.org/downloads.html) 
 - http://spark.apache.org/docs/2.3.2/
 - [typesafe](https://github.com/lightbend/config) for config
+- [spark-fast-tests](https://github.com/MrPowers/spark-fast-tests) for DataSet|DataFrame compare function
+
     
 
 
--- spark/src/test/scala/com/nberserk/spark/example/AppTest.scala
+++ spark/src/test/scala/com/nberserk/spark/example/AppTest.scala
@@ -1,11 +1,12 @@
 package com.nberserk.spark.example
 
+import com.github.mrpowers.spark.fast.tests.DatasetComparer
 import grizzled.slf4j.Logging
 import org.apache.spark.sql.SparkSession
 import org.scalatest.{Outcome, fixture}
 
 
-class AppTest extends fixture.FunSuite with Logging {
+class AppTest extends fixture.FunSuite with Logging with DatasetComparer {
   type FixtureParam = SparkSession
 
   def withFixture(test: OneArgTest): Outcome = {
@@ -23,7 +24,23 @@ class AppTest extends fixture.FunSuite with Logging {
     assert(empsRDD.count === 4)
   }
 
+  test("dataset"){ spark =>
+    import spark.implicits._
+    val sourceDF = Seq(
+      ("jose"),
+      ("li"),
+      ("luisa")
+    ).toDF("name")
 
+    val actualDF = sourceDF.select( sourceDF.col("name").alias("student"))
+    val expectedDF = Seq(
+      ("jose"),
+      ("li"),
+      ("luisa")
+    ).toDF("student")
+
+    assertSmallDatasetEquality(actualDF, expectedDF)
+  }
 
 
 }
